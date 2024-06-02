79bd350e60bbf5cc4cd43b3fee344359176f0786
leeixi <david.levaicc@gmail.com>
2024-06-02 15:10:24 +0200

    make db config more clearer

--- itemboard/itemboard/settings.py
+++ itemboard/itemboard/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'itemboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #    'default': {
@@ -93,12 +93,6 @@ WSGI_APPLICATION = 'itemboard.wsgi.application'
 #    }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
