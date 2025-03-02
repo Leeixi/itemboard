735a261c33938e9b5e7cb081d92ea60c79ff1142
David Lee <david.levacicc@gmail.com>
2025-03-02 18:24:00 +0100

    Removed NAT GW and private subnet association

--- IaC/VPC/main.tf
+++ IaC/VPC/main.tf
@@ -42,25 +42,6 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-# Elastic IP for NAT Gateway
-resource "aws_eip" "nat" {
-  domain = "vpc"
-
-  tags = {
-    Name = "itemboard-nat-eip"
-  }
-}
-
-# NAT Gateway for private subnets
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
-
-  tags = {
-    Name = "itemboard-nat-gateway"
-  }
-}
-
 # Route table for public subnets
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -75,32 +56,11 @@ resource "aws_route_table" "public" {
   }
 }
 
-# Route table for private subnets
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "itemboard-private-route-table"
-  }
-}
-
 # Associate public subnets with public route table
 resource "aws_route_table_association" "public" {
   count          = 2
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
-}
-
-# Associate private subnets with private route table
-resource "aws_route_table_association" "private" {
-  count          = 2
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
 }
 
 # Security group for itemboard tasks
